55b1cb01518235e9a93132f52c93a19a19a4f9cc
freedMJ <1071705638@qq.com>
2019-04-16 20:27:06 +0800

    开启登录拦截功能

---
+++ src/main/java/com/lmj/train/interceptor/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.lmj.train.interceptor;
+
+import com.lmj.train.model.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+        User user = (User) request.getSession().getAttribute("user");
+        if(user!=null){
+            return true;
+        }else{
+            response.sendRedirect(request.getContextPath()+"/login");
+            return false;
+        }
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
